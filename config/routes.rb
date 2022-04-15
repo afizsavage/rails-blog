cf0bb49da159f18b2208adb5040dbdda7b862554
Afiz Savage <savageafiz9@gmail.com>
2022-04-15 15:11:28 +0000

    Added functionality to add comment to post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'likes/create'
+  get 'comments/create'
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create]
+    resources :posts, only: %i[index show new create] do
+      resources :comments, only: %i[create new]
+      resources :likes, only: %i[create]
+    end
   end
 
   root to: 'users#index'
