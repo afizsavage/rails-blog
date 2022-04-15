68772b4de5a06f8c8c4d1929fbd7957de88eb98b
Afiz Savage <savageafiz9@gmail.com>
2022-04-15 08:51:30 +0000

    Initialized index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   class UsersController < ApplicationController
-    def index; end
+    def index
+      @users = User.all
+    end
 
-    def show; end
+    def show
+      @user = User.find(params[:id])
+    end
   end
 end
