2f83a54dff0bd72d641648e86104340d45fa82ad
Afiz Savage <savageafiz9@gmail.com>
2022-04-16 09:39:27 +0000

    Refactored request specs

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,34 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    before(:each) { get '/users' }
-    it 'has a 200 success status code' do
-      expect(response).to have_http_status(200)
-    end
-
-    it 'renders the correct template' do
-      expect(response).to render_template(:index)
-    end
-
-    it 'should render the correct text in the template' do
-      expect(response.body).to include('Here is the list of our users')
+  describe 'GET users' do
+    before(:each) do
+      it 'has a 200 success status code' do
+        expect(response).to have_http_status(200)
+      end
+      it 'If a correct template was rendered' do
+        expect(response).to render_template(:index)
+      end
     end
   end
-
-  describe 'GET /users/:id' do
-    before(:example) { get '/users/1' }
-
-    it 'should return 200 http_status_code' do
-      expect(response).to have_http_status(:ok)
-    end
-
-    it 'should render the correct template' do
-      expect(response).to render_template(:show)
-    end
-
-    it 'should render the correct text in the template' do
-      expect(response.body).to include('Here is a single user')
+  describe 'GET single user' do
+    before(:each) do
+      it 'has a 200 success status code' do
+        expect(response).to have_http_status(200)
+      end
+      it 'If a correct template was rendered' do
+        expect(response).to render_template(:show)
+      end
     end
   end
 end
